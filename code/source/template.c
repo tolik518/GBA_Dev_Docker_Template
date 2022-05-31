e55919d5eafc031aa510fe77ecc4724962d4b516
Anatolij Vasilev <Anatolij.Vasilev@flyeralarm.com>
2022-05-31 10:37:44 +0200

    resetted the templace.c

--- code/source/template.c
+++ code/source/template.c
@@ -1,82 +1,18 @@
-#include <tonc.h>
+include <tonc.h>
 
-void drawline(int x1, int y1, int x2, int y2, int color);
-void drawCube(int x0, int y0, int x1, int y1, int color);
-int diff(int a, int b);
-int zero(int a, int b);
+int main(void) {
 
-int main(void) 
-{
-	REG_DISPCNT = DCNT_MODE3 | DCNT_BG2;
+	REG_DISPCNT = DCNT_MODE0 | DCNT_BG0;
 
-	/*int y = 239;
-	int x = 159;*/
+	tte_init_chr4c_default(0, BG_CBB(0) | BG_SBB(31));
+	tte_set_pos(92, 68);
+	tte_write("Hello World!");
 
-	int color = 16;
-	while (1) 
-	{
-		drawCube(5,5,154,234, color);
-		/*for(int i = 0; i <= x/2; i++)
-		{
-			drawCube(i, i, x-i, y-i, color);
-		}*/
-		//VBlankIntrWait();
-		
-		++color;
+	irq_init(NULL);
+	irq_enable(II_VBLANK);
+
+	while (1) {
+		VBlankIntrWait();
 	}
-}
 
-int diff(int a, int b) 
-{
-	if (a > b)
-        return a - b;
-    else
-        return b - a;
-}
-
-int zero(int a, int b) 
-{
-	if (a > b )
-        return a;
-    else
-        return b;
-}
-
-void drawline(int x0, int y0, int x1, int y1, int color) 
-{
-	int dx = diff(x0, x1);
-	int sx = x0 < x1 ? 1 : -1;
-	int dy = -diff(y0, y1);
-	int sy = y0 < y1 ? 1 : -1;
-	int error = dx + dy;
-
-	while(true)
-	{
-		m3_mem[x0][y0] = color;
-
-		if (x0 == x1 && y0 == y1) break;
-
-		int e2 = 2 * error;
-
-		if (e2 >= dy){
-			if (x0 == x1) break;
-			error = error + dy;
-			x0 = x0 + sx;
-		}
-
-		if (e2 <= dx){
-			if (y0 == y1) break;
-			error = error + dx;
-			y0 = y0 + sy;
-		}
-	}
-}
-
-
-void drawCube(int x0, int y0, int x1, int y1, int color)
-{
-	drawline(x1, y0, x0, y0, color);
-	drawline(x0, y1, x0, y0, color);
-	drawline(x0, y1, x1, y1, color);
-	drawline(x1, y0, x1, y1, color);
 }
